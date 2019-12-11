44e7724e677eb53ff05bbd92c1932a8b2b5dce3a
Raphael Amorim <rapha850@gmail.com>
2019-12-10 23:45:59 -0300

    update 013

--- challenge-013.js
+++ challenge-013.js
@@ -1,16 +1,27 @@
 /*
 
-Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+Write a function that reverses a string. The input string is given as an array of characters char[].
 
-You may assume that each input would have exactly one solution, and you may not use the same element twice.
+Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
 
-Example:
+You may assume all the characters consist of printable ascii characters.
+
+Example 1:
 
 ```
-Given nums = [2, 7, 11, 15], target = 9,
+Input: ["h","e","l","l","o"]
+Output: ["o","l","l","e","h"]
+```
 
-Because nums[0] + nums[1] = 2 + 7 = 9,
-return [0, 1].
+Example 2:
+
+```
+Input: ["H","a","n","n","a","h"]
+Output: ["h","a","n","n","a","H"]
 ```
 
 */
+
+var reverseString = function(s) {
+  s.reverse();
+};
